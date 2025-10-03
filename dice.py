1d10f4cd315fe3d38eeca4f6bd060331ae991afb
Gregory Freeman <gsfreemanjr@gmail.com>
2025-10-03 02:03:02 -0400

    10-03-2025 2:03am

--- dice.py
+++ dice.py
@@ -1,23 +1,49 @@
 import random
 
+class Dice:
+    def __init__(self, sides):
+        self.sides = sides
+    def roll(self):
+        return random.randint(1, self.sides)
 
-def dice_roller():
-    while True: 
+d4 = Dice(4)
+d6 = Dice(6)
+d8 = Dice(8)
+d10 = Dice(10)
+d12 = Dice(12)
+d20 = Dice(20)
+d100 = Dice(100)
+dice_case = [4, 6, 8, 10, 12, 20, 100,]
+
+def main():
+    print('Pick a dice to roll. When you are done type 0 to exit.')
+    while True:
         try:
-            roll_quantity = int(input('How many rolls do you need-> #').strip())
-        except:
-                print('please try again')
+            selected_dice = int(input('d: ').strip().lower())
+            if selected_dice == 0:
+                raise SyntaxError
+            elif selected_dice not in dice_case:
+                raise ValueError
+        except ValueError:
+            print('Dice not available')
+        except SyntaxError:
+            break
         else:
-                break
-    for _ in range(roll_quantity):
-        while True:
-            try: 
-                dice_size = int(input('Select a dice size-> d:').strip())
-            except:
-                print('please try again')
-            else:
-                break
-        print(f'd{dice_size}: {random.randint(1, dice_size)}')
+            match selected_dice:
+                case 4:
+                    print(d4.roll())
+                case 6:
+                    print(d6.roll())                       
+                case 8:
+                    print(d8.roll())                     
+                case 10:
+                    print(d10.roll())                    
+                case 12:
+                    print(d12.roll())                    
+                case 20:
+                    print(d20.roll()) 
+                case 100:
+                    print(d100.roll())
 
-if __name__ == '__main__':
-    dice_roller()
+if __name__ == "__main__":
+    main()
